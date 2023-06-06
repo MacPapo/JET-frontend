8d1aa0246540229108c553a1b824b1917e831763
Visco01 <visconti373@gmail.com>
2023-06-06 12:04:58 +0200

    Home Entity components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,10 @@ import { HomeComponent } from './components/home/home.component';
 
 // Custom Modules
 import { MaterialModule } from './material/material.module';
+import { WaiterHomeComponent } from './components/waiter/waiter-home/waiter-home.component';
+import { CookerHomeComponent } from './components/cooker/cooker-home/cooker-home.component';
+import { BartenderHomeComponent } from './components/bartender/bartender-home/bartender-home.component';
+import { CashierHomeComponent } from './components/cashier/cashier-home/cashier-home.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +28,10 @@ import { MaterialModule } from './material/material.module';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
+    WaiterHomeComponent,
+    CookerHomeComponent,
+    BartenderHomeComponent,
+    CashierHomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/cooker/cooker-home/cooker-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cooker-home',
+  templateUrl: './cooker-home.component.html',
+  styleUrls: ['./cooker-home.component.css']
+})
+export class CookerHomeComponent {
+
+}
