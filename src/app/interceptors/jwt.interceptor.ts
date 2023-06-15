c70ce0c4b2dd9d90882fdfb53b0f1c935edf0937
Visco01 <visconti373@gmail.com>
2023-06-15 21:48:41 +0200

    Infinite loop fix

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,70 +1,90 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpRequest,
-    HttpHandler,
-    HttpEvent,
-    HttpInterceptor,
-    HTTP_INTERCEPTORS
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { JwtService } from '../services/auth/jwt.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  private isRefreshing = false;
 
-    constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const accessToken = this.jwtService.getLoginData()?.accessToken;
-        if (accessToken) {
-            request = this.addAuthorizationHeader(request, accessToken);
+  // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  //   const accessToken = this.jwtService.getLoginData()?.accessToken;
+  //   if (accessToken) {
+  //     request = this.addAuthorizationHeader(request, accessToken);
+  //   }
+
+  //   return next.handle(request).pipe(
+  //     catchError((error) => {
+  //       if (error.status === 401) {
+  //         return this.handleUnauthorizedError(request, next);
+  //       }
+  //       return throwError(error);
+  //     })
+  //   );
+  // }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const accessToken = this.jwtService.getLoginData()?.accessToken;
+    if (accessToken) {
+      req = this.addAuthorizationHeader(req, accessToken);
+    }
+
+    return next.handle(req).pipe(
+      catchError((error) => {
+        if (error.status === '401') {
+          return this.handleUnauthorizedError(req, next);
         }
+        return throwError(() => error);
+      })
+    );
+  }
 
-        return next.handle(request).pipe(
-            catchError((error) => {
-                if (error.status === 401) {
-                    return this.handleUnauthorizedError(request, next);
-                }
-                return throwError(error);
-            })
-        );
-    }
+  private addAuthorizationHeader(request: HttpRequest<any>, accessToken: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+  }
 
-    private addAuthorizationHeader(request: HttpRequest<any>, accessToken: string): HttpRequest<any> {
-        return request.clone({
-            setHeaders: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-    }
+  private handleUnauthorizedError(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return this.jwtService.refreshToken().pipe(
+      switchMap((response) => {
+        console.log(response);
+        this.isRefreshing = false;
+        this.jwtService.setTokens(response.accessToken, response.refreshToken);
+        const updatedRequest = this.addAuthorizationHeader(request, response.accessToken);
+        return next.handle(updatedRequest);
+      }),
+      catchError((error: any) => {
+        this.isRefreshing = false;
+        if (error.status === 401) {
+          this.jwtService.logout();
+        }
+        return throwError(error);
+      })
+    );
+  }
 
-    private handleUnauthorizedError(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return this.jwtService.refreshToken().pipe(
-            switchMap((response) => {
-                this.jwtService.setTokens(response.accessToken, response.refreshToken);
-                const updatedRequest = this.addAuthorizationHeader(request, response.accessToken);
-                return next.handle(updatedRequest);
-            }),
-            catchError((error: any) => {
-                if (this.isRefreshTokenExpiredError(error)) {
-                    console.log("DAI CHE ELIMINIAMO IL BRO");
-                    this.jwtService.logout();
-                }
-                return throwError(error);
-            })
-        );
-    }
-
-    private isRefreshTokenExpiredError(error: any): boolean {
-        return (error.statusCode === '10001' || error.statusCode === 10001)
-            && (error.message === 'RefreshTokenExpired' || error.message === 'jwt expired');
-    }
+  private isRefreshTokenExpiredError(error: any): boolean {
+    return (error.error.statusCode === '10001' || error.error.statusCode === 10001)
+      && (error.error.message === 'RefreshTokenExpired' || error.error.message === 'jwt expired');
+  }
 }
 
 export const JwtInterceptorProvider = {
-    provide:  HTTP_INTERCEPTORS,
-    useClass: JwtInterceptor,
-    multi:    true,
+  provide: HTTP_INTERCEPTORS,
+  useClass: JwtInterceptor,
+  multi: true,
 };
--- src/app/services/auth/jwt.service.ts
+++ src/app/services/auth/jwt.service.ts
@@ -4,172 +4,177 @@ import { Observable, BehaviorSubject, of } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { environment as env } from '../../../environments/environment';
 import jwt_decode from 'jwt-decode';
+import { Router } from '@angular/router';
 
 interface Role {
-    _id: string;
-    code: string;
+  _id: string;
+  code: string;
 }
 
 interface LoginResponse {
-    data: {
-        tokens: {
-            accessToken: string;
-            refreshToken: string;
-        };
-        user: {
-            _id: string;
-            roles: Role[];
-        };
+  data: {
+    tokens: {
+      accessToken: string;
+      refreshToken: string;
     };
+    user: {
+      _id: string;
+      roles: Role[];
+    };
+  };
 }
 
 interface RefreshTokenResponse {
-    accessToken: string;
-    refreshToken: string;
+  accessToken: string;
+  refreshToken: string;
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class JwtService {
-    private apiUrl = '/api/auth';
+  private apiUrl = '/api/auth';
 
-    private loginUrl = `${this.apiUrl}/login`;
-    private refreshTokenUrl = `${this.apiUrl}/token/refresh`;
+  private loginUrl = `${this.apiUrl}/login`;
+  private refreshTokenUrl = `${this.apiUrl}/token/refresh`;
 
-    private headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'x-api-key': env.xApiKey
-    });
+  private headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'x-api-key': env.xApiKey
+  });
 
-    refreshSubject = new BehaviorSubject<boolean>(false);
-    private refreshingToken = false;
+  refreshSubject = new BehaviorSubject<boolean>(false);
+  private refreshingToken = false;
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private router: Router) {}
 
-    login(email: string, password: string): Observable<any> {
-        return this.http
-            .post<LoginResponse>(this.loginUrl,
-                                 { email, password },
-                                 { headers: this.headers })
-            .pipe(
-                tap(response => {
-                    this.saveLoginData(
-                        response.data.tokens.accessToken,
-                        response.data.tokens.refreshToken,
-                        response.data.user._id,
-                        response.data.user.roles
-                    );
-                })
-            );
+  login(email: string, password: string): Observable<any> {
+    return this.http
+      .post<LoginResponse>(this.loginUrl,
+        { email, password },
+        { headers: this.headers })
+      .pipe(
+        tap(response => {
+          this.saveLoginData(
+            response.data.tokens.accessToken,
+            response.data.tokens.refreshToken,
+            response.data.user._id,
+            response.data.user.roles
+          );
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('roles');
+    this.router.navigate(['/login']);
+  }
+
+  refreshToken(): Observable<any> {
+    const refreshToken = this.getLoginData().refreshToken;
+    return this.http
+      .post<RefreshTokenResponse>(
+        this.refreshTokenUrl,
+        { refreshToken },
+        { headers: this.headers }
+      )
+      .pipe(
+        tap(
+          response => {
+            console.log('refresh token');
+            this.setTokens(response.accessToken, response.refreshToken);
+            this.refreshSubject.next(true);
+          },
+          error => {
+            console.log(error.error.message);
+            console.log('refresh token error');
+          }
+        )
+      );
+  }
+
+  private saveLoginData(
+    accessToken: string,
+    refreshToken: string,
+    userId: string,
+    roles: Role[]
+  ): void {
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+    localStorage.setItem('userId', userId);
+
+    const roleCodes = roles.map(role => role.code);
+
+    localStorage.setItem('roles', JSON.stringify(roleCodes));
+  }
+
+  setTokens(accessToken: string, refreshToken: string): void {
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('refreshToken', refreshToken);
+  }
+
+  getLoginData(): any {
+    return {
+      accessToken: localStorage.getItem('accessToken'),
+      refreshToken: localStorage.getItem('refreshToken'),
+      userId: localStorage.getItem('userId'),
+      roles: JSON.parse(localStorage.getItem('roles') || '[]')
+    };
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    const loginData = this.getLoginData();
+    if (!loginData) {
+      return of(false);
     }
-
-    logout(): void {
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('refreshToken');
-        localStorage.removeItem('userId');
-        localStorage.removeItem('roles');
+    if (!loginData.refreshToken || !loginData.accessToken) {
+      return of(false);
     }
-
-    refreshToken(): Observable<any> {
-        const refreshToken = this.getLoginData().refreshToken;
-        return this.http
-            .post<RefreshTokenResponse>(
-                this.refreshTokenUrl,
-                { refreshToken },
-                { headers: this.headers }
-            )
-            .pipe(
-                tap(
-                    response => {
-                        this.setTokens(response.accessToken, response.refreshToken);
-                        this.refreshSubject.next(true);
-                    },
-                    error => {
-                        console.log(error.error.message);
-                    }
-                )
-            );
-    }
-    
-    private saveLoginData(
-        accessToken: string,
-        refreshToken: string,
-        userId: string,
-        roles: Role[]
-    ): void {
-        localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('refreshToken', refreshToken);
-        localStorage.setItem('userId', userId);
-
-        const roleCodes = roles.map(role => role.code);
-
-        localStorage.setItem('roles', JSON.stringify(roleCodes));
-    }
-
-    setTokens(accessToken: string, refreshToken: string): void {
-        localStorage.setItem('accessToken', accessToken);
-        localStorage.setItem('refreshToken', refreshToken);
-    }
-
-    getLoginData(): any {
-        return {
-            accessToken: localStorage.getItem('accessToken'),
-            refreshToken: localStorage.getItem('refreshToken'),
-            userId: localStorage.getItem('userId'),
-            roles: JSON.parse(localStorage.getItem('roles') || '[]')
-        };
-    }
-
-    isLoggedIn(): Observable<boolean> {
-        const loginData = this.getLoginData();
-        if (!loginData) {
+    if (this.isTokenExpired(loginData.accessToken)) {
+      if (!this.refreshingToken) {
+        this.refreshingToken = true;
+        return this.refreshToken().pipe(
+          tap(() => {
+            this.refreshingToken = false;
+            this.refreshSubject.next(true);
+          }),
+          catchError(() => {
+            this.refreshingToken = false;
+            this.refreshSubject.next(false);
             return of(false);
-        }
-        if (!loginData.refreshToken || !loginData.accessToken) {
-            return of(false);
-        }
-        if (this.isTokenExpired(loginData.accessToken)) {
-            if (!this.refreshingToken) {
-                this.refreshingToken = true;
-                return this.refreshToken().pipe(
-                    tap(() => {
-                        this.refreshingToken = false;
-                        this.refreshSubject.next(true);
-                    }),
-                    catchError(() => {
-                        this.refreshingToken = false;
-                        this.refreshSubject.next(false);
-                        return of(false);
-                    })
-                );
-            }
-        }
-        return this.refreshSubject.asObservable();
+          })
+        );
+      }
     }
+    return this.refreshSubject.asObservable();
+  }
 
 
 
-    isAdmin(): boolean {
-        const loginData = this.getLoginData();
-        return loginData.roles && loginData.roles.includes('ADMIN');
+  isAdmin(): boolean {
+    const loginData = this.getLoginData();
+    return loginData.roles && loginData.roles.includes('ADMIN');
+  }
+
+  private getDecodedAccessToken(token: string): any {
+    try {
+      return jwt_decode(token);
+    } catch (error) {
+      return null;
     }
+  }
 
-    private getDecodedAccessToken(token: string): any {
-        try {
-            return jwt_decode(token);
-        } catch (error) {
-            return null;
-        }
+  isTokenExpired(token: string): boolean {
+    const decodedToken = this.getDecodedAccessToken(token);
+    if (decodedToken) {
+      const expireDate = new Date(decodedToken.exp * 1000).getDate();
+      const currentDate = new Date().getDate();
+      return expireDate < currentDate;
     }
-
-    isTokenExpired(token: string): boolean {
-        const decodedToken = this.getDecodedAccessToken(token);
-        if (decodedToken) {
-            const expireDate = new Date(decodedToken.exp * 1000).getDate();
-            const currentDate = new Date().getDate();
-            return expireDate < currentDate;
-        }
-        return true;
-    }
+    return true;
+  }
 }
