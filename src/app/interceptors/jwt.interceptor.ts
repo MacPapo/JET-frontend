cbe4643cec0a5bcbc1dc52b6ceb2c555a719c5e5
MacPapo <jacopocostantini32@gmail.com>
2023-06-16 14:26:39 +0200

    Fixed Intrceptor

--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor,
-  HTTP_INTERCEPTORS
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor,
+    HTTP_INTERCEPTORS
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
@@ -14,77 +14,65 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  private isRefreshing = false;
+    private isRefreshing = false;
 
-  constructor(private jwtService: JwtService) {}
+    constructor(private jwtService: JwtService) {}
+ 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const accessToken = this.jwtService.getLoginData()?.accessToken;
+        if (accessToken) {
+            req = this.addAuthorizationHeader(req, accessToken);
+        }
 
-  // intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-  //   const accessToken = this.jwtService.getLoginData()?.accessToken;
-  //   if (accessToken) {
-  //     request = this.addAuthorizationHeader(request, accessToken);
-  //   }
-
-  //   return next.handle(request).pipe(
-  //     catchError((error) => {
-  //       if (error.status === 401) {
-  //         return this.handleUnauthorizedError(request, next);
-  //       }
-  //       return throwError(error);
-  //     })
-  //   );
-  // }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const accessToken = this.jwtService.getLoginData()?.accessToken;
-    if (accessToken) {
-      req = this.addAuthorizationHeader(req, accessToken);
+        return next.handle(req).pipe(
+            catchError((error) => {
+                if (error instanceof HttpErrorResponse && error.status === 401) {
+                    return this.handleUnauthorizedError(req, next);
+                }
+                return throwError(() => error);
+            })
+        );
     }
 
-    return next.handle(req).pipe(
-      catchError((error) => {
-        if (error.status === '401') {
-          return this.handleUnauthorizedError(req, next);
+    private addAuthorizationHeader(request: HttpRequest<any>, accessToken: string): HttpRequest<any> {
+        return request.clone({
+            setHeaders: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+    }
+
+    private handleUnauthorizedError(request: HttpRequest<any>, next: HttpHandler) {
+        if (!this.isRefreshing) {
+            this.isRefreshing = true;
+
+            if (this.jwtService.isLoggedIn()) {
+                return this.jwtService.refreshToken().pipe(
+                    switchMap((response) => {
+                        this.isRefreshing = false;
+                        this.jwtService.setTokens(response.accessToken, response.refreshToken);
+                        const updatedRequest = this.addAuthorizationHeader(request, response.accessToken);
+                        return next.handle(updatedRequest);
+                    }),
+                    catchError((error) => {
+                        this.isRefreshing = false;
+
+                        if (error.error.statusCode === '10001') {
+                            this.jwtService.logout();
+                        }
+                        
+                        return throwError(() => error);
+                    })
+                );
+            }
         }
-        return throwError(() => error);
-      })
-    );
-  }
 
-  private addAuthorizationHeader(request: HttpRequest<any>, accessToken: string): HttpRequest<any> {
-    return request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    });
-  }
-
-  private handleUnauthorizedError(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return this.jwtService.refreshToken().pipe(
-      switchMap((response) => {
-        console.log(response);
-        this.isRefreshing = false;
-        this.jwtService.setTokens(response.accessToken, response.refreshToken);
-        const updatedRequest = this.addAuthorizationHeader(request, response.accessToken);
-        return next.handle(updatedRequest);
-      }),
-      catchError((error: any) => {
-        this.isRefreshing = false;
-        if (error.status === 401) {
-          this.jwtService.logout();
-        }
-        return throwError(error);
-      })
-    );
-  }
-
-  private isRefreshTokenExpiredError(error: any): boolean {
-    return (error.error.statusCode === '10001' || error.error.statusCode === 10001)
-      && (error.error.message === 'RefreshTokenExpired' || error.error.message === 'jwt expired');
-  }
+        return next.handle(request);
+    }
 }
 
 export const JwtInterceptorProvider = {
-  provide: HTTP_INTERCEPTORS,
-  useClass: JwtInterceptor,
-  multi: true,
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true,
 };
